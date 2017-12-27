7fbbf6e3c7165707fbbfe5aeb83a52850a9c045c
OguzDoger <oguzdoger@gmail.com>
2017-12-27 23:15:35 +0300

    Initial Commit

---
+++ FitCiti/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FitCiti
+//
+//  Created by Oğuz Döğer on 27.12.2017.
+//  Copyright © 2017 BoshWare. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
